66ba99098f62ce72e8ed6f4fa1e19c6d61692ee8
molliemac <mollie.mcintyre9@gmail.com>
2017-01-15 19:47:34 -0800

    Checkpoint 4 COMPLETE

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -15,10 +15,14 @@
          .state('album', {
              url: '/album',
              templateUrl: '/templates/album.html'
+         })
+         
+         .state('collection', {
+             url: '/collection',
+             templateUrl: '/templates/collection.html'
          });
      }
  
- angular.module('blocJams', ['ui.router']);
      angular
          .module('blocJams', ['ui.router'])
          .config(config);
